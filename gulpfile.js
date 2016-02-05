ee54834f2fd724f31ec317fbd706116421701814
110chang <u@110chang.com>
2016-02-05 16:43:56 +0900

    [change] output bundle by env

--- gulpfile.js
+++ gulpfile.js
@@ -13,23 +13,42 @@ var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 var minimist = require('minimist');
 
+var knownOptions = {
+  string: 'env',
+  default: { env: process.env.NODE_ENV || 'development' } // NODE_ENVに指定がなければ開発モードをデフォルトにする
+};
+
+// コマンドラインの入力を解析
+var options = minimist(process.argv.slice(2), knownOptions);
+var isProduction = (options.env === 'production') ? true : false;
+
+//console.log('[build env]', options.env, '[is production]', isProduction);
+
 gulp.task('bundle', function() {
-  browserify({
+  var b = browserify({
     entries: ['./src/main.jsx'],
     extensions: ['.jsx', '.es6'],
-    debug: true
+    debug: true//!isProduction
   }).transform(babelify, {
     presets: ['es2015', 'react', 'stage-2'],
     extensions: ['.jsx', '.es6']
   })
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    //.pipe(uglify())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./dist'))
-    .pipe(browsersync.stream());
+
+  if (isProduction) {
+    b.bundle()
+      .pipe(source('bundle.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(uglify())
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./dist'))
+      .pipe(browsersync.stream());
+  } else {
+    b.bundle()
+      .pipe(source('bundle.js'))
+      .pipe(gulp.dest('./dist'))
+      .pipe(browsersync.stream());
+  }
 });
 
 gulp.task('server', function() {
@@ -46,6 +65,5 @@ gulp.task('default', ['server', 'bundle'], function() {
   gulp.watch('./dist/**/*', function() {
     browsersync.reload();
   });
-  console.log(process.env.NODE_ENV);
 });
 
